66cc96a16c4d279c0ec166fe80028fcf72e3163c
Shanthakumar <Shanthakumar@ngws37.local>
2018-04-10 09:37:07 +0530

    Initial Commit

---
+++ SQliteTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SQliteTest
+//
+//  Created by Shanthakumar on 10/04/18.
+//  Copyright © 2018 Shanthakumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
